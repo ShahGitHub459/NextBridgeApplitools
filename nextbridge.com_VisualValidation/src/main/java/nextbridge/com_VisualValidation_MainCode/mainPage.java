97e2673bf18d4688134b746fa2c4686564c96f58
ShahGitHub459 <61537965+ShahGitHub459@users.noreply.github.com>
2020-03-03 15:27:01 +0500

    commit M

--- nextbridge.com_VisualValidation/src/main/java/nextbridge/com_VisualValidation_MainCode/mainPage.java
+++ nextbridge.com_VisualValidation/src/main/java/nextbridge/com_VisualValidation_MainCode/mainPage.java
@@ -39,7 +39,7 @@ public class mainPage {
 	//Main Page 
 	@Test(priority=0)
   public void mainPageOpening() {
-	  driver.get("https://nextbridge.com/");
+	  driver.get("https://nextbridge.com/robotics-machine-design/");
 	  while(isPresent==false) {
 		  isPresent=driver.findElements(objectsRequired.mainPageCheckingObject).size() > 0;
 	  }
@@ -47,152 +47,130 @@ public class mainPage {
 	//Check point Applitools
 	  eyes.open(driver,"Next","Main Page", new RectangleSize(626,600));
 	  eyes.setForceFullPageScreenshot(true);
-	  //eyes.checkWindow("Main Page");
 	  	
   }
 	@Test(priority=1)
 	public void servicesHover() {
-		//((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", objectsRequired.servicesObject());
-		//actions.moveToElement(objectsRequired.servicesObject()).perform();
-		//Check point Applitools
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/a[2]/i")).click();
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[1]/a/div/span/i")).click();
-		//eyes.setForceFullPageScreenshot(true);
-		//eyes.checkWindow("Main Page");
 	}
 	@Test(priority=2)
-	public void technologiesHover() {
-		//actions.moveToElement(objectsRequired.technologiesObject()).perform();
-		//Check point Applitools	
+	public void technologiesHover() {	
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[2]/a/div/span/i")).click();
-		//eyes.setForceFullPageScreenshot(true);
-		//eyes.checkWindow("Main Page");
 	}
 	@Test(priority=3)
 	public void looInsideHover() {
-		//actions.moveToElement(objectsRequired.lookInsideObject()).perform();
-		//Check point Applitools	
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[3]/a/div/span/i")).click();
-		//eyes.setForceFullPageScreenshot(true);
+		//Check point Applitools	
 		js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
 		js.executeScript("window.scrollTo(0, -document.body.scrollHeight);");
 		eyes.checkWindow("Main Page");
 	}
 	@Test(priority=4)
 	public void aboutClick() throws InterruptedException {
-		//actions.moveToElement(objectsRequired.aboutObject()).perform();
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[4]/a/span")).click();
 		//Check point Applitools	
-		//eyes.setForceFullPageScreenshot(true);
 		Thread.sleep(5000);
 		js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
 		js.executeScript("window.scrollTo(0, -document.body.scrollHeight);");
 		Thread.sleep(5000);
 		eyes.checkWindow("About Page");
 	}
-/*	@Test(priority=5)
+	/*@Test(priority=5)
 	public void blogClick() {
 		actions.moveToElement(objectsRequired.blogObject()).perform();
 		//Check point Applitools	
-	}
-	*/
-	@Test(priority=6)
+	}*/
+	
+	@Test(priority=5)
 	public void contactClick() throws InterruptedException {
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/a[2]/i")).click();
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[6]/a/span")).click();
 		//Check point Applitools	
-		//eyes.setForceFullPageScreenshot(true);
 		Thread.sleep(5000);
 		js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
 		js.executeScript("window.scrollTo(0, -document.body.scrollHeight);");
 		Thread.sleep(5000);
 		eyes.checkWindow("Contact Page");
 	}
-	@Test(priority=7)
+	@Test(priority=6)
 	public void servicesSoftwareDevelopment() throws InterruptedException {
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/a[2]/i")).click();
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[1]/a/div/span/i")).click();
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[1]/ul/li[1]/a/span")).click();
 		//Check point Applitools	
-		//eyes.setForceFullPageScreenshot(true);
 		Thread.sleep(5000);
 		js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
 		js.executeScript("window.scrollTo(0, -document.body.scrollHeight);");
 		Thread.sleep(5000);
 		eyes.checkWindow("servicesSoftwareDevelopment Page");
 	}
-	@Test(priority=8)
+	@Test(priority=7)
 	public void specializedVerticals() throws InterruptedException {
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/a[2]/i")).click();
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[1]/a/div/span/i")).click();
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[1]/ul/li[2]/a/span")).click();
 		//Check point Applitools	
-		//eyes.setForceFullPageScreenshot(true);
 		Thread.sleep(5000);
 		js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
 		js.executeScript("window.scrollTo(0, -document.body.scrollHeight);");
 		Thread.sleep(5000);
 		eyes.checkWindow("specializedVerticals Page");
 	}
-	@Test(priority=9)
+	@Test(priority=8)
 	public void roboticsSoftware() throws InterruptedException {
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/a[2]/i")).click();
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[1]/a/div/span/i")).click();
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[1]/ul/li[3]/a/span")).click();
 		//Check point Applitools	
-		//eyes.setForceFullPageScreenshot(true);
 		Thread.sleep(5000);
 		js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
 		js.executeScript("window.scrollTo(0, -document.body.scrollHeight);");
 		Thread.sleep(5000);
 		eyes.checkWindow("roboticsSoftware Page");
 	}
-	@Test(priority=10)
+	@Test(priority=9)
 	public void roboticsMachineDesign() throws InterruptedException {
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/a[2]/i")).click();
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[1]/a/div/span/i")).click();
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[1]/ul/li[4]/a/span")).click();
 		//Check point Applitools	
-		//eyes.setForceFullPageScreenshot(true);
 		Thread.sleep(5000);
 		js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
 		js.executeScript("window.scrollTo(0, -document.body.scrollHeight);");
 		Thread.sleep(5000);
 		eyes.checkWindow("roboticsMachineDesign Page");
 	}
-	@Test(priority=11)
+	@Test(priority=10)
 	public void technologiesLanguageAndFramework() throws InterruptedException {
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/a[2]/i")).click();
-		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/a[2]/i")).click();
+		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[2]/a/div/span/i")).click();
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[2]/ul/li/a/span")).click();
 		//Check point Applitools	
-		//eyes.setForceFullPageScreenshot(true);
 		Thread.sleep(5000);
 		js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
 		js.executeScript("window.scrollTo(0, -document.body.scrollHeight);");
 		Thread.sleep(5000);
 		eyes.checkWindow("technologiesLanguageAndFramework Page");
 	}
-	@Test(priority=12)
+	@Test(priority=11)
 	public void lookInsideInitiatives() throws InterruptedException {
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/a[2]/i")).click();
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[3]/a/div/span/i")).click();
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[3]/ul/li[1]/a/span")).click();
 		//Check point Applitools	
-		//eyes.setForceFullPageScreenshot(true);
 		Thread.sleep(5000);
 		js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
 		js.executeScript("window.scrollTo(0, -document.body.scrollHeight);");
 		Thread.sleep(5000);
 		eyes.checkWindow("lookInsideInitiatives Page");
 	}
-	@Test(priority=13)
+	@Test(priority=12)
 	public void lookInsideEvents() throws InterruptedException {
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/a[2]/i")).click();
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[3]/a/div/span/i")).click();
 		driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/div/ul/li[3]/ul/li[2]/a/span")).click();
 		//Check point Applitools	
-		//eyes.setForceFullPageScreenshot(true);
 		Thread.sleep(5000);
 		js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
 		js.executeScript("window.scrollTo(0, -document.body.scrollHeight);");
